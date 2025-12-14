25a148fd3aecfcbce1368381a5f959940a5a1209
Alok Singh <aloks0583@gmail.com>
2025-12-14 20:49:44 +0530

    Initial Commit

---
+++ EventCounter/EventCounterApp.swift
@@ -0,0 +1,32 @@
+//
+//  EventCounterApp.swift
+//  EventCounter
+//
+//  Created by Alok SIngh on 14/12/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct EventCounterApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
